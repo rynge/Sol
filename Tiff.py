8518fb98dc8bb90ba3b1fc48e8846c1c63617416
N R Callahan <nicholas.r.callahan@gmail.com>
2015-01-09 13:33:50 -0700

    Added region detector and warp methods

--- Tiff.py
+++ Tiff.py
@@ -1,9 +1,16 @@
 import os
 import sys 
 import re 
-from math import pow
+from math import pow, floor
 from subprocess import Popen, PIPE
 class Tiff:
+##--DAYMET Default Data
+    DAYMET_proj="+proj=lcc +lat_1=25 +lat_2=60 +lat_0=42.5 +lon_0=-100 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"
+    stLat=50
+    stLon=-132
+    stTile=12625
+##--PRISM Default Data
+    PRISM_proj=""
     def __init__(self, path,filename):
         self.filename=filename
         self.location=path
@@ -12,6 +19,10 @@ class Tiff:
         self.nPixelY=0
         self.projCoords=list()
         self.deciCoords=list()
+        self.DAYMET_tile=list()
+        self.loadTiff()
+        self.calculateDAYMETTile()
+        self.calculateRegion()
     def loadTiff(self):
         if not os.path.isfile(self.filepath):
             sys.exit("File does not exist, or permissions are incorrect")
@@ -61,7 +72,34 @@ class Tiff:
                     lon -= float(match.group(j + 3)) / pow(60, j)
                     lat += float(match.group(j + 6)) / pow(60, j)
                 self.deciCoords.append((lat, lon))
-    
+    def calculateDAYMETTile(self):
+    ##--Tiff Data
+        ulLat=self.deciCoords[1][0]
+        ulLon=self.deciCoords[1][1]
+        lrLat=self.deciCoords[0][0]
+        lrLon=self.deciCoords[0][1]
+    ##--Calculations
+        ulLat = self.stLat + floor((ulLat - self.stLat) / 2) * 2
+        ulLon = self.stLon + floor((ulLon - self.stLon) / 2) * 2
+        lrLat = self.stLat + floor((lrLat - self.stLat) / 2) * 2
+        lrLon = self.stLon + floor((lrLon - self.stLon) / 2) * 2
+        ulTile = int(self.stTile + ((ulLat - self.stLat) / 2) * 180 + (ulLon - self.stLon) / 2)
+        self.DAYMET_tile.append(ulTile)
+        lrTile = int(self.stTile + ((lrLat - self.stLat) / 2) * 180 + (lrLon - self.stLon) / 2)
+        self.DAYMET_tile.append(lrTile)
+    def calculateRegion(self):
+        command = ['gdalinfo', self.filepath]
+        process=Popen(command, stdout=PIPE, shell=False)
+        stdout,stderr=process.communicate()
+        if process.returncode != 0:
+            print stderr
+            sys.exit("Could not open " + self.filename)
+        stdout = stdout.split('\n')
+        for line in stdout:
+            if line.startswith('    AUTHORITY'):
+                line=line.translate(None, '[]"/')
+                line = line.split(',')
+                self.region=line[1]
     def mergeTiff(self,other,path,output):
         if not os.path.isfile(other):
             sys.exit("File " + other + " does not exist")
@@ -81,4 +119,22 @@ class Tiff:
             new_tiff=Tiff(path,output)
             return new_tiff
         else:
-            print "File " + output + " already exists. Exiting"       
+            print "File " + output + " already exists. Exiting"      
+    def warp(self,proj):
+        if proj=="DAYMET":
+            print "Converting to DAYMET Projection"
+            t_proj=self.DAYMET_proj
+        elif proj=="PRISM":
+            print "Converting to PRISM Projection"
+            t_proj=self.PRISM_proj
+        else:
+            raise RuntimeError("Invalid projection type")
+        command = ['gdalwarp', '-s_srs', 'EPSG:' + self.region, '-overwrite', '-t_srs',t_proj, '-r', 'bilinear', '-of', 'GTiff', '-tr', '10', '-10']
+        output_file=self.filename[:-4]+"_converted.tif"
+        output_file=os.path.join(self.location,output_file)
+        command.append(self.filename)
+        command.append(output_file)
+        process = Popen(command,stdout=PIPE,shell=False)
+        stderr=process.communicate()
+        if process.returncode != 0:
+            sys.exit(stderr)
