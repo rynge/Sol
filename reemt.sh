8153297cc6af621fa3bb6e0127137b80adbdbcca
N R Callahan <nicholas.r.callahan@gmail.com>
2015-04-23 10:31:36 -0700

    EEMT should work

--- reemt.sh
+++ reemt.sh
@@ -10,12 +10,31 @@ TMIN=$6
 TMAX=$7
 VP=$8
 PRCP=$9
-TWI=$10
-TOTAL_SUN=$11
-FLAT_SUN=$12
-SLOPE=$13
-ASPECT=$14
+TWI=${10}
+TOTAL_SUN=${11}
+HOURS_SUN=${12}
+FLAT_SUN=${13}
+SLOPE=${14}
+ASPECT=${15}
 
+
+echo "================================================================================"
+echo "Proj_dir: $DIRECTORY"
+echo "Month: $MONTH"
+echo "Year: $YEAR"
+echo "DEM: $DEM"
+echo "NA_DEM: $NA_DEM"
+echo "TMin: $TMIN"
+echo "Tmax: $TMAX"
+echo "Vp: $VP"
+echo "PRCP: $PRCP"
+echo "TWI: $TWI"
+echo "Total_Sun: $TOTAL_SUN"
+echo "Hours_SunL $HOURS_SUN"
+echo "Flat_Sun: $FLAT_SUN"
+echo "Slope: $SLOPE"
+echo "Aspect: $ASPECT"
+echo "================================================================================"
 WORKING_DIR=$RANDOM 
 LOCATION=${DIRECTORY}/sol_data/tmp_${WORKING_DIR}/PERMANENT
 GRASSRC=${DIRECTORY}/.grassrc_${WORKING_DIR}
@@ -82,62 +101,93 @@ g.region -s rast=dem_10m
 
 #Import DAYMET Data - tmin,tmax,twi,prcp,vp
 echo "Importing DAYMET Data"
+echo "TMIN"
 r.in.gdal input=${TMIN} output=tmin
+echo "TMAX"
 r.in.gdal input=${TMAX} output=tmax
+echo "TWI"
 r.in.gdal input=${TWI} output=twi
+echo "PRCP"
 r.in.gdal input=${PRCP} output=prcp
+echo "VP"
 r.in.gdal input=${VP} output=vp
 
 #Import r.sun results
+echo "Total Sun"
 r.in.gdal input=${TOTAL_SUN} output=total_sun
-r.in.gdal input=${FLAT_SUN} output=flat_total_sun
+echo "Hours Sun"
+r.in.gdal input=${HOURS_SUN} output=hours_sun
+echo "Slope"
 r.in.gdal input=${SLOPE} output=slope
+echo "Aspect"
 r.in.gdal input=${ASPECT} output=aspect
 
 #NA_DEM
+echo "NA_DEM"
 r.in.gdal input=${NA_DEM} output=dem_1km
 
+#Create Flat map
+echo "Creating Zeros Map"
+r.mapcalc "zeros=if(dem_10m>0,0,null())"
+echo "Create Flat Sun Map"
+#r.sun elevin=dem_10m aspin=zeros slopein=zeros day="1" step="0.05" dist="1" glob_rad=flat_total_sun  
+r.in.gdal input=$DIRECTORY/flat.tif output=flat_total_sun
+
+
 #Locally Corrected Temperature
+echo "Locally Corrected Temp"
 r.mapcalc "tmin_loc = tmin-0.00649*(dem_10m-dem_1km)"
 r.mapcalc "tmax_loc = tmax-0.00649*(dem_10m-dem_1km)"
 
+
 #Local Potential Evapotranspiration for EEMT-Trad
+echo "Local Potential Evapotranspiration for EEMT-Trad"
+
 r.mapcalc "f_tmin_loc = 6.108*exp((17.27*tmin_loc)/(tmin_loc+273.3))"
 r.mapcalc "f_tmax_loc = 6.108*exp((17.27*tmax_loc)/(tmax_loc+273.3))"
 r.mapcalc "vp_s = (f_tmax_loc+f_tmin_loc)/2"
 r.mapcalc "PET = 2.1*((hours_sun/12)^2)*vp_s/((tmax_loc+tmin_loc)/2)"
 
 #Local Solar Insolation
+echo "#Local Solar Insolation"
 r.mapcalc "total_sun_joules = total_sun*3600"
 
 #Locally Corrected Temperature (accounting for Solar Insolation)
+echo "#Locally Corrected Temperature (accounting for Solar Insolation)"
 r.mapcalc "S_i = total_sun/flat_total_sun"
 r.mapcalc "tmin_topo = tmin_loc+(S_i-(1/S_i))"
 r.mapcalc "tmax_topo = tmax_loc+(S_i-(1/S_i))"
 
 #Local Water Balance (accounting for topographic water redistribution)
+echo "Local Water Balance"
 r.mapcalc "a_i = twi/((max(twi)+min(twi))/2)"
 
 #Potential Evapotranspiration for EEMT-Topo
+echo "Potential Evapotranspiration EEMT-Topo"
 r.mapcalc "g_psy = 0.001013*(101.*((293-0.00649*dem_10m)/293)^5.26)"
 r.mapcalc "m_vp = 0.04145*exp(0.06088*(tmax_topo+tmin_topo/2))"
-r.mapcalc "ra = (4.72*(ln(2/0.00137))2)/(1+0.536*5)"
-r.mapcalc "vp_loc = 6.11*10(7.5*tmin_topo)/(237.3+tmin_topo)"
+r.mapcalc "ra = (4.72*(log(2/0.00137))*2)/(1+0.536*5)"
+r.mapcalc "vp_loc = 6.11*10*(7.5*tmin_topo)/(237.3+tmin_topo)"
 r.mapcalc "f_tmin_topo = 6.108*exp((17.27*tmin_topo)/(tmin_topo+237.3))"
 r.mapcalc "f_tmax_topo = 6.108*exp((17.27*tmin_topo)/(tmin_topo+237.3))"
 r.mapcalc "vp_s_topo = (f_tmax_topo+f_tmin_topo)/2"
-r.mapcalc "p_a = 101325*exp(-9.80665*0.289644*dem_10m/(8.31447*288.15))/287.35*((tmax_topo+tmin_topo/2)273.125)"
-r.mapcalc "PET = total_sun_joules+p_a*0.001013*(vp_s_topo-vp_loc)/ra))/(2.45*(m_vp+g_psy)"
-r.mapcalc "AET = prcp*(1+PET/prcp(1(PET/prcp)2.63)(1/2.63))"
+echo "p_a"
+r.mapcalc "p_a = 101325*exp(-9.80665*0.289644*dem_10m/(8.31447*288.15))/287.35*((tmax_topo+tmin_topo/2)*273.125)"
+echo "PET"
+r.mapcalc "PET = (total_sun_joules+p_a*0.001013*((vp_s_topo-vp_loc)/ra))/(2.45*(m_vp+g_psy))"
+echo "AET"
+r.mapcalc "AET = prcp*(1+PET/prcp*(1*(PET/prcp)*82.63)*(1/2.63))"
 
 #EEMT-Traditional
-r.mapcalc "PET_Trad = (2.1*((hours_sun/12)^2*vp_s/((tmax_loc+tmin_loc)/2)"
+echo "EEMT-TRad"
+r.mapcalc "PET_Trad = (2.1*(hours_sun/12)^2*vp_s)/((tmax_loc+tmin_loc)/2)"
 r.mapcalc "E_ppt_trad = prcp - PET_Trad"
 r.mapcalc "NPP_trad = 3000*(1+exp(1.315-0.119*(tmax_loc+tmin_loc)/2)^-1)"
 r.mapcalc "E_bio_trad = NPP_trad*(22*10^6)"
 r.mapcalc "EEMT_Trad = E_ppt_trad + E_bio_trad"
 
 #EEMT-Topographical
+echo "EEMT-Topo"
 r.mapcalc "F = a_i*prcp"
 r.mapcalc "DT = ((tmax_topo+tmin_topo)/2) - 273.15"
 r.mapcalc "N = cos(slope*0.0174532925)*sin(aspect*0.0174532925)"
@@ -147,5 +197,8 @@ r.mapcalc "E_ppt_topo =F*4185.5*DT*E_bio_topo"
 r.mapcalc "EEMT_Topo = E_ppt_topo + E_bio_topo"
 
 
-r.out.gdal -c createopt="TFW=YES,COMPRESS=LZW" input=EEMT_Trad output=eemt/yearly/${YEAR}/${MONTH}/EEMT_Trad_${MONTH}_${YEAR}.tif
-r.out.gdal -c createopt="TFW=YES,COMPRESS=LZW" input=EEMT_Topo output=eemt/yearly/${YEAR}/${MONTH}/EEMT_Topo_${MONTH}_${YEAR}.tif
+r.out.gdal -c createopt="TFW=YES,COMPRESS=LZW" input=EEMT_Topo output=${DIRECTORY}/EEMT_Topo_${MONTH}_${YEAR}.tif
+r.out.gdal -c createopt="TFW=YES,COMPRESS=LZW" input=EEMT_Trad output=${DIRECTORY}/EEMT_Trad_${MONTH}_${YEAR}.tif
+r.out.gdal -c createopt="TFW=YES,COMPRESS=LZW" input=flat_total_sun output=${DIRECTORY}/flat.tif
+
+rm -rf ${DIRECTORY}/sol_data
